0f5073f7815aaf7bd1b0b908b8b0c64d5474a0be
Armaan Amoli <91266111+ArmaanAmoli@users.noreply.github.com>
2025-12-24 20:15:14 +0530

    API Test

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
 import { OpenRouter } from "@openrouter/sdk";
 import { restClient } from "@massive.com/client-js";
-const OpenRouter = new OpenRouter({
-    apiKey: ''
-})
+import "dotenv/config";
+const openrouter = new OpenRouter({
+    apiKey: process.env.OPEN_ROUTER_API_KEY
+});
+
+
+// Testing API link
+
+const stream = await openrouter.chat.send({
+  model: "deepseek/deepseek-r1-0528:free",
+  messages: [
+    {
+      "role": "user",
+      "content": "APPL current price"
+    }
+  ],
+  stream: true
+});
+
+for await (const chunk of stream) {
+  const content = chunk.choices[0]?.delta?.content;
+  if (content) {
+    process.stdout.write(content);
+  }
+}
+// console.log(typeof(stream))
+// console.log(stream)
