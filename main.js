0d098833252c48fb90e5922cb3cd3f05a3b92d28
Armaan Amoli <91266111+ArmaanAmoli@users.noreply.github.com>
2025-12-25 19:51:50 +0530

    update

--- main.js
+++ main.js
@@ -1,29 +1,47 @@
 import { OpenRouter } from "@openrouter/sdk";
 import { restClient } from "@massive.com/client-js";
+import { AreaSeries, createChart } from 'lightweight-charts';
 import "dotenv/config";
 const openrouter = new OpenRouter({
-    apiKey: process.env.OPEN_ROUTER_API_KEY
+  apiKey: process.env.OPEN_ROUTER_API_KEY
 });
 
+// FinancialDataAPI_Key = process.env.FINANCIAL_DATA_API_KEY_BY_MASSIVE;
 
-// Testing API link
+const RestClientDataAPI = restClient(process.env.FINANCIAL_DATA_API_KEY_BY_MASSIVE, 'https://api.massive.com');
 
-const stream = await openrouter.chat.send({
-  model: "deepseek/deepseek-r1-0528:free",
-  messages: [
-    {
-      "role": "user",
-      "content": "APPL current price"
-    }
-  ],
-  stream: true
-});
-
-for await (const chunk of stream) {
-  const content = chunk.choices[0]?.delta?.content;
-  if (content) {
-    process.stdout.write(content);
+// making a request for historic stock data.
+async function getHistoricStockData(ticker) {
+  try{
+    const response = await RestClientDataAPI.getStocksAggregates(
+      {
+        stocksTicker:ticker,
+        multiplier:"1",
+        timespan:"day",
+        from:"2025-12-01",
+        to:"2025-12-25",
+        adjusted:"true",
+        sort:"asc",
+        limit: "120"
+      }
+    );
+    console.log(response);
+  }catch(e){
+    console.log('An error happened: ' ,e);
   }
 }
-// console.log(typeof(stream))
-// console.log(stream)
+
+
+//Time and close price data;
+let DatePriceObjectList = []
+let priceData = getHistoricStockData("AAPL").results;
+for(const priceObject in priceData){
+  DatePriceObjectList.push({time:new Date(priceObject.t), value:priceObject.c});
+}
+
+const chartOptions = { layout: { textColor: 'white', background: { type: 'solid', color: 'black' } } };
+const chart = createChart(document.querySelector('.content'),chartOptions);
+const areaSeries = chart.addSeries(AreaSeries,{lineColor: '#2962FF', topColor: '#2962FF',
+    bottomColor: 'rgba(41, 98, 255, 0.28)',});
+    areaSeries.setData(DatePriceObjectList);
+chart.timeScale().fitContent();
