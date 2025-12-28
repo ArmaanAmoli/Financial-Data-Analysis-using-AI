3f41afcc2bcecad7dfb7bf92831b7560d468dc67
Armaan Amoli <91266111+ArmaanAmoli@users.noreply.github.com>
2025-12-28 20:56:24 +0530

    Adding some important utility functions

--- main.js
+++ main.js
@@ -1,47 +1,68 @@
 import { OpenRouter } from "@openrouter/sdk";
 import { restClient } from "@massive.com/client-js";
-import { AreaSeries, createChart } from 'lightweight-charts';
 import "dotenv/config";
+// import { stream } from "@openrouter/sdk/lib/matchers.js";
 const openrouter = new OpenRouter({
   apiKey: process.env.OPEN_ROUTER_API_KEY
 });
 
 // FinancialDataAPI_Key = process.env.FINANCIAL_DATA_API_KEY_BY_MASSIVE;
-
 const RestClientDataAPI = restClient(process.env.FINANCIAL_DATA_API_KEY_BY_MASSIVE, 'https://api.massive.com');
-
 // making a request for historic stock data.
-async function getHistoricStockData(ticker) {
+async function getHistoricStockData(ticker, DateToday , DateYearAgo) {
   try{
     const response = await RestClientDataAPI.getStocksAggregates(
       {
         stocksTicker:ticker,
         multiplier:"1",
         timespan:"day",
-        from:"2025-12-01",
-        to:"2025-12-25",
+        from:DateYearAgo,
+        to:DateToday,
         adjusted:"true",
         sort:"asc",
         limit: "120"
       }
     );
-    console.log(response);
+    return response;
   }catch(e){
-    console.log('An error happened: ' ,e);
+    return e;
   }
 }
 
-
-//Time and close price data;
-let DatePriceObjectList = []
-let priceData = getHistoricStockData("AAPL").results;
-for(const priceObject in priceData){
-  DatePriceObjectList.push({time:new Date(priceObject.t), value:priceObject.c});
+async function LLM(tick , data , today , YearBack){
+  const response = await openrouter.chat.send({
+    model:"xiaomi/mimo-v2-flash:free",
+    messages:[
+      {"role":"user",
+        "content":`Stock Ticker: ${tick}\nFrom Date ${YearBack} to ${today}\nProvide analysis and Investment Advice for the given data\n${data}`
+      }
+    ],
+    stream:false}
+  )
+  // return response.choices[0]?.delta?.content;
+  return response;
+  
 }
 
-const chartOptions = { layout: { textColor: 'white', background: { type: 'solid', color: 'black' } } };
-const chart = createChart(document.querySelector('.content'),chartOptions);
-const areaSeries = chart.addSeries(AreaSeries,{lineColor: '#2962FF', topColor: '#2962FF',
-    bottomColor: 'rgba(41, 98, 255, 0.28)',});
-    areaSeries.setData(DatePriceObjectList);
-chart.timeScale().fitContent();
+function FormatDate(date){
+    return date.toISOString().slice(0,10);
+}
+
+async function generate(Tick){
+  let todayDate = new Date();
+  let OneYearAgo = new Date(todayDate);
+  OneYearAgo.setFullYear(todayDate.getFullYear()-1);
+  todayDate = FormatDate(todayDate);
+  OneYearAgo= FormatDate(OneYearAgo);
+  const StockData = await getHistoricStockData(Tick,todayDate,OneYearAgo);
+  const responseFromAi = await LLM(Tick , StockData , todayDate , OneYearAgo)
+  const para = document.querySelector(".ResponsePara");
+  para.textContent = responseFromAi.choices[0].message.content;
+}
+
+const AnalyseButton = document.querySelector(".submit-button");
+const Ticker = document.querySelector(".ticker");
+AnalyseButton.addEventListener("click" , function(){
+  let Tick = Ticker.ariaValueMax;
+  generate(Tick);
+});
